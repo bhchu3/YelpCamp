743bf47983828c1f1394e1a5127a539a509118dd
Billy Chui <bhchu3@hotmail.com>
2021-08-29 00:04:18 +1000

    - add image in models/campground - add image and description in show.ejs

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDb = async () => {
   await CampGround.deleteMany();
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 31) + 10;
     const camp = new CampGround({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${randomArray(descriptors)} ${randomArray(places)}`,
+      image: "https://source.unsplash.com/collection/483251/800x600",
+      price,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime labore exercitationem culpa veritatis libero repellat eum error, ad, quia rem corrupti necessitatibus rerum accusantium quam voluptatibus saepe. Minima dicta est facilis accusamus cupiditate recusandae autem libero ratione similique! Minus maiores labore earum velit odit veniam quis nulla expedita optio sint debitis ipsam sequi nemo, aut incidunt dolore nam cupiditate possimus, iusto numquam reiciendis dolores eaque provident. Blanditiis omnis enim suscipit eaque provident, quidem voluptates error quam. Iusto a id officia accusantium et optio, earum, ducimus voluptate ut sed autem voluptas, doloremque distinctio pariatur facilis quas tempora recusandae quisquam. Alias, nesciunt.",
     });
     await camp.save();
   }
