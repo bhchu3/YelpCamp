1adfe70e06b93983bb02f32d5d6267a46b50befb
Billy Chui <bhchu3@hotmail.com>
2021-09-09 18:02:31 +1000

    set up campground Delete Middleware using mongoose

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
+const { func } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./reviews");
 
 // Create a Schema
 const campGroundSchema = new Schema({
@@ -14,6 +16,18 @@ const campGroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// Campground Delete Middleware
+campGroundSchema.post("findOneAndDelete", async function (doc) {
+  console.log(doc);
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 // compiling our schema into a Model.
