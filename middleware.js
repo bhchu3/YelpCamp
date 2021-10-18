69c405a1e9b55bd2dfcfebd9debabbf1b16947f8
Billy Chui <bhchu3@hotmail.com>
2021-10-18 22:58:31 +1100

    add req.user in app.js app.use, setting logic for the sign up link navbar

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ module.exports.isLoggedIn = (req, res, next) => {
   // .isAuthenticated() is a method from passport.js
   // this function checks if the user is successfully logged in;
   // if it is, then it returns true, otherwise, it returns false.
+  console.log("REQ.USER...", req.user);
   if (!req.isAuthenticated()) {
     req.flash("error", "You must sign in");
     return res.redirect("/login");
